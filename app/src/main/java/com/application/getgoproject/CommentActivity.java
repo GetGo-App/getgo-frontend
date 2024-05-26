d8afcfb535d9471f8fbfb681a87d2df5e584bd18
ngongo <thuhuong562002@gmail.com>
2024-05-26 23:55:05 +0700

    comment, account

---
+++ app/src/main/java/com/application/getgoproject/CommentActivity.java
@@ -0,0 +1,52 @@
+package com.application.getgoproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+
+public class CommentActivity extends AppCompatActivity {
+    ListView lvComment;
+    ArrayList<Comment> arrayComment;
+    CommentAdapter adapter;
+    private ImageButton imgbtnGoback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_comment);
+
+        anhXa();
+
+        adapter = new CommentAdapter(this, R.layout.layout_cmt, arrayComment);
+        lvComment.setAdapter(adapter);
+        imgbtnGoback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                homeForm();
+            }
+        });
+
+    }
+    private void anhXa(){
+        imgbtnGoback = findViewById(R.id.imgbtnGoback);
+        lvComment = (ListView) findViewById(R.id.lvComment);
+        arrayComment = new ArrayList<>();
+        arrayComment.add(new Comment(R.drawable.border_gradient, "Yen Vi", "2 days ago", "Awesome place for tourist trying to have a sip of coffee. Authentic taste, quiet with beautiful vintage decorations. Recommended.", R.drawable.star_rate));
+        arrayComment.add(new Comment(R.drawable.border_gradient, "Duc Anh", "a week ago", "Beautiful view, very nice atmostphere, the food so good and cheap. Love it!", R.drawable.star_rate));
+        arrayComment.add(new Comment(R.drawable.border_gradient, "Mimi", "a week ago", "Nice people, nice view, nice price", R.drawable.star_rate));
+    }
+
+    private void homeForm(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/application/getgoproject/CommentAdapter.java
@@ -0,0 +1,61 @@
+package com.application.getgoproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CommentAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private List<Comment> commentList;
+
+    public CommentAdapter(Context context, int layout, List<Comment> commentList) {
+        this.context = context;
+        this.layout = layout;
+        this.commentList = commentList;
+    }
+
+    @Override
+    public int getCount() {
+        return commentList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(layout, null);
+
+        //ánh xạ view
+        TextView name = (TextView) convertView.findViewById(R.id.tvName);
+        TextView time = (TextView) convertView.findViewById(R.id.tvTime);
+        TextView comment = (TextView) convertView.findViewById(R.id.tvComment);
+        ImageView avatar = (ImageView) convertView.findViewById(R.id.imgAvatar);
+        ImageView star = (ImageView) convertView.findViewById(R.id.imgAvatar);
+
+        //gan gia tri
+        Comment comments = commentList.get(position);
+        name.setText(comments.getName());
+        time.setText(comments.getTime());
+        comment.setText(comments.getComment());
+        avatar.setImageResource(comments.getAvatar());
+        star.setImageResource(comments.getStar());
+
+        return convertView;
+    }
+}
