a39920d11f95778d53db7865068485b3274da841
HieuNghia0000 <fakenamemc1@gmail.com>
2024-05-27 17:20:26 +0700

    add camera and google maps feature

---
+++ app/src/main/java/com/application/getgoproject/MapsActivity.java
@@ -0,0 +1,42 @@
+package com.application.getgoproject;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.application.getgoproject.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng nhaVanHoa = new LatLng(10.875159, 106.800542);
+        mMap.addMarker(new MarkerOptions().position(nhaVanHoa).title("Nhà Văn Hóa Sinh Viên"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(nhaVanHoa, 17));
+    }
+}
