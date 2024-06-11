57461c2cc968679ad4ca5611824c29d2b43a5c44
HieuNghia0000 <fakenamemc1@gmail.com>
2024-06-11 16:34:07 +0700

    chat agent can send location

--- app/src/main/java/com/application/getgoproject/models/ChatBox.java
+++ app/src/main/java/com/application/getgoproject/models/ChatBox.java
@@ -2,15 +2,33 @@ package com.application.getgoproject.models;
 
 public class ChatBox {
     private String text;
+    private Locations location;
     private boolean isSent;
+
+    public ChatBox(String text) {
+        this.text = text;
+        this.isSent = true;
+        this.location = null;
+    }
 
     public ChatBox(String text, boolean isSent) {
         this.text = text;
         this.isSent = isSent;
+        this.location = null;
+    }
+
+    public ChatBox(Locations location) {
+        this.text = null;
+        this.isSent = false; // Received message
+        this.location = location;
     }
 
     public String getText() {
         return text;
+    }
+
+    public Locations getLocation() {
+        return location;
     }
 
     public boolean isSent() {
