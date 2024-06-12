52528b05bf7047958d6d80bd9a36670b72cb9364
HieuNghia0000 <fakenamemc1@gmail.com>
2024-06-12 17:31:07 +0700

    update random show discover in main

--- app/src/main/java/com/application/getgoproject/MainActivity.java
+++ app/src/main/java/com/application/getgoproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.application.getgoproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -14,20 +15,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.application.getgoproject.callback.LocationCallback;
+import com.application.getgoproject.models.Locations;
 import com.application.getgoproject.models.UserAuthentication;
 import com.application.getgoproject.service.LocationService;
 import com.application.getgoproject.utils.RetrofitClient;
 import com.application.getgoproject.utils.SharedPrefManager;
+import com.bumptech.glide.Glide;
 import com.google.android.material.imageview.ShapeableImageView;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
     private ShapeableImageView avatar;
     private ImageView imgPlace, imgBanner;
     private ImageButton btnAssistant, btnStatus, btnQr, imgAddStory;
-    private TextView tvUsername;
+    private TextView tvUsername, textPlace;
     private LocationService locationService;
+    private ArrayList<Locations> arrayLocation;
+    private String userToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +52,14 @@ public class MainActivity extends AppCompatActivity {
 
         Retrofit retrofit = RetrofitClient.getRetrofitInstance(this);
         locationService = retrofit.create(LocationService.class);
+        arrayLocation = new ArrayList<>();
 
         mapping();
 
         UserAuthentication userAuthentication = SharedPrefManager.getInstance(this).getUser();
         if (userAuthentication != null) {
             String username = userAuthentication.getUsername();
+            userToken = "Bearer " + userAuthentication.getAccessToken();
             tvUsername.setText(username);
         } else {
             Toast.makeText(this, "User not logged in", Toast.LENGTH_SHORT).show();
@@ -95,6 +112,23 @@ public class MainActivity extends AppCompatActivity {
                 cameraForm();
             }
         });
+
+        getAllLocations(userToken, new LocationCallback() {
+            @Override
+            public void onLocationsFetched(List<Locations> locations) {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onOneLocationsFetched(Locations locations) {
+
+            }
+        });
     }
     private void mapping(){
         avatar = findViewById(R.id.avatar);
@@ -105,6 +139,7 @@ public class MainActivity extends AppCompatActivity {
         btnQr = findViewById(R.id.btnQr);
         imgAddStory =findViewById(R.id.imgAddStory);
         tvUsername = findViewById(R.id.username);
+        textPlace = findViewById(R.id.textPlace);
     }
     private void packageForm(){
         Intent intent = new Intent(this, PackageActivity.class);
@@ -144,6 +179,84 @@ public class MainActivity extends AppCompatActivity {
         if (userAuthentication != null) {
             String name = userAuthentication.getUsername();
             tvUsername.setText(name);
+        }
+    }
+
+    private void getAllLocations(String token, LocationCallback callback) {
+        Call<List<Locations>> call = locationService.getAllLocations(token);
+        call.enqueue(new Callback<List<Locations>>() {
+            @Override
+            public void onResponse(Call<List<Locations>> call, Response<List<Locations>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<Locations> locationsList = response.body();
+
+                    arrayLocation.clear();
+                    mergeLocationsByCity(locationsList);
+
+                    Log.d("Array Size", arrayLocation.size() + "");
+
+                    // Set random location image and city
+                    if (!arrayLocation.isEmpty()) {
+                        setRandomLocation();
+                    }
+
+                    callback.onLocationsFetched(arrayLocation);
+                } else {
+                    Log.d("Error", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Locations>> call, Throwable throwable) {
+                Log.d("Failed!", throwable.getMessage());
+            }
+        });
+    }
+
+    private void setRandomLocation() {
+        Random random = new Random();
+        int locationIndex = random.nextInt(arrayLocation.size());
+        Locations randomLocation = arrayLocation.get(locationIndex);
+
+        List<String> images = randomLocation.getImages();
+        String imageUrl = null;
+        if (images != null && !images.isEmpty()) {
+            int imageIndex = random.nextInt(images.size());
+            imageUrl = images.get(imageIndex);
+        }
+
+        String cityName = randomLocation.getCity();
+
+        if (imageUrl != null && !imageUrl.isEmpty()) {
+            Glide.with(this)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.sapa)
+                    .into(imgPlace);
+        } else {
+            imgPlace.setImageResource(R.drawable.sapa);
+        }
+
+        textPlace.setText(cityName);
+    }
+
+
+    private void mergeLocationsByCity(List<Locations> locationsList) {
+        Set<String> uniqueCities = new HashSet<>();
+
+        arrayLocation.clear();
+
+        for (Locations location : locationsList) {
+            String city = location.getCity();
+            if (!uniqueCities.contains(city)) {
+                uniqueCities.add(city);
+                // Add only the city name and minimal information to arrayLocation
+                arrayLocation.add(new Locations(location.getId(), location.getName(), location.getAddress(),
+                        location.getContent(), city, location.getLatitude(), location.getLongitude(),
+                        location.getImages(), location.isAvailable(), location.getOpenTime(),
+                        location.getDetailUrl(), location.getHotline(), location.getPrice(),
+                        location.getCategoryId(), location.getRating(), location.getWebsiteRating(),
+                        location.getWebsiteRatingOverall(), location.isTrend(), location.isTopYear()));
+            }
         }
     }
 }
